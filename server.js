68f818bf082273ff0d9ee0f337d20b41733b79a0
William Berry <76190311+wberry86@users.noreply.github.com>
2021-04-29 17:44:44 -0500

    updated candidates routes to join with party data

--- server.js
+++ server.js
@@ -30,7 +30,11 @@ const db = mysql.createConnection(
 
 // get all candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id`;
 
 db.query(sql, (err, rows) => {
     if (err) {
@@ -47,7 +51,12 @@ db.query(sql, (err, rows) => {
 
 // Get a single candidate
 app.get('/api/candidate/:id', (req, res) => {
-  const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name 
+    AS party_name 
+    FROM candidates 
+    LEFT JOIN parties 
+    ON candidates.party_id = parties.id 
+    WHERE candidates.id = ?`;
   const params = [req.params.id];
 
   db.query(sql, params, (err, row) => {
