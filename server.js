a37450ccbf4fc7fd5ece9a069aa6b09091768e88
William Berry <76190311+wberry86@users.noreply.github.com>
2021-04-28 21:15:22 -0500

    added create, delete, select candidates to detebase functionality

--- server.js
+++ server.js
@@ -28,10 +28,45 @@ const db = mysql.createConnection(
     },
     console.log('Connected to the election daatabase')
 );
-
+/*
 db.query(`SELECT * FROM candidates`, (err, rows) => {
     console.log(rows);
   });
+
+
+// get a single candidate
+
+
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(row);
+});
+
+
+
+// delete a candidate
+db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});
+
+*/
+
+// create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+                VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
