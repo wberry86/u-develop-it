5a4c29960febd1e6d7ce45f385f29bb05d84cc01
William Berry <76190311+wberry86@users.noreply.github.com>
2021-04-28 20:40:17 -0500

    connected to database

--- server.js
+++ server.js
@@ -1,18 +1,37 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+
 
 // express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 
-// Default response for any other request (Not Found)
+// default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
 });
 
+
+
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'SammyLyla3328',
+        database: 'election'
+    },
+    console.log('Connected to the election daatabase')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
