e68adc597bef3da7aedd5928b3ddd428cfac5ea5
William Berry <76190311+wberry86@users.noreply.github.com>
2021-05-03 19:46:20 -0500

    added voter routes

--- routes/apiRoutes/partyRoutes.js
+++ routes/apiRoutes/partyRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../../db/connection');
+const inputCheck = require('../../utils/inputCheck');
 
 
 router.get("/parties", (req, res) => {
